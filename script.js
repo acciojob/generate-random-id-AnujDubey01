b5b1800b6e6bf19eb005626b305db96868e6acc0
Anuj Dubey <149352591+AnujDubey01@users.noreply.github.com>
2025-05-21 20:18:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function makeid(l) {
 	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
 	for ( let i = 0 ; i < l ; i++){
-	result += characters.charAt(Math.floor(math.random() * characters.length));
+	result += characters.charAt(Math.floor(Math.random() * characters.length));
 	}
 	return result;
 }
